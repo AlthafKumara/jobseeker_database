16207cad1433d064874df9777edfa5d9ec0fa646
AlthafKumara <amartyacoalthaf01@gmail.com>
2025-10-12 18:13:28 +0700

    Update auth

--- routes/auth.js
+++ routes/auth.js
@@ -198,7 +198,7 @@ router.post(
       check('name', 'Company name is required').not().isEmpty(),
       check('address', 'Address is required').not().isEmpty(),
       check('phone', 'Phone is required').not().isEmpty(),
-      check('description''Description is required').not().isEmpty(),
+      check('description','Description is required').not().isEmpty(),
       check('logo', 'logo isrequired').not().isEmpty()
     ]
   ],
