d2f89bd4a14b0df7ed32122bc566e495ee3fb46e
AlthafKumara <amartyacoalthaf01@gmail.com>
2025-10-13 10:12:59 +0700

    update multer image

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const User = require('../models/user.model');
 const Society = require('../models/society.model');
 const Company = require('../models/company.model');
 const { auth } = require('../middleware/auth');
+const {upload} = require('../middleware/upload');
 
 const router = express.Router();
 
